b558fae4e58ffc5c6b9bd69b9a07893014557b34
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-01-02 20:39:08 +0900

    Title: 선택 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240102/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[105];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int mn = min_element(arr + i, arr + n) - arr;
+
+        int tmp = arr[i];
+        arr[i] = arr[mn];
+        arr[mn] = tmp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
