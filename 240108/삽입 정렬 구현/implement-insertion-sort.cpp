3a2c02927be62e24cdbe95bac838081d41a08562
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-01-08 16:01:19 +0900

    Title: 삽입 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[105];
+
+int main() {
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }   
+
+    for(int i = 1; i < n; i++){
+        int j = i - 1;
+        int key = arr[i];
+
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
