eee358579c2beea83c2e79fdd1f849265500dc3d
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-01-09 15:58:21 +0900

    Title: 기수 정렬 구현, Time: 70ms, Memory: 2MB, Status: Passed - CodeTree

--- 240109/기수 정렬 구현/implement-radix-sort.cpp
+++ 240109/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,55 +1,61 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector<int> arr;
-int n;
 
-int extract_digit(int idx, int k) {
-	if (k == 0)
-		return arr[idx] % (10 * (k + 1));
-	else
-		return (arr[idx] % (10 * (k + 1))) / (10 * k);
+
+int extractDigit(int num, int pos) {
+    while (pos > 0) {
+        num /= 10;
+        pos--;
+    }
+    return num % 10;
+}
+void radixSort(vector<int>& arr, int k) {
+    for (int pos = 0; pos < k; pos++) {
+        vector<int> arr_new[10];
+        for (int i = 0; i < arr.size(); i++) {
+            int digit = extractDigit(arr[i], pos);
+            arr_new[digit].push_back(arr[i]);
+        }
+
+        vector<int> store_arr;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < arr_new[i].size(); j++) {
+                store_arr.push_back(arr_new[i][j]);
+            }
+        }
+
+        arr = store_arr;
+    }
 }
 
-void radix_sort(int k) {
-	for (int pos = 0; pos < k; pos++) {
-		vector<int> arr_new[10];
-		for (int i = 0; i < arr.size(); i++) {
-			int digit = extract_digit(i, pos);
-			arr_new[digit].push_back(arr[i]);
-		}
+int main() {
+    int n, num;
 
-		vector<int> store_arr;
+    cin >> n;
 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < arr_new[i].size(); j++) {
-				store_arr.push_back(arr_new[i][j]);
-			}
-		}
-		
-		arr = store_arr;
-	}
-	
-}
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        arr.push_back(num);
+    }
+    int maxDigits = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        int num = arr[i];
+        int digits = 0;
+        while (num > 0) {
+            num /= 10;
+            digits++;
+        }
+        maxDigits = max(maxDigits, digits);
+    }
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+        radixSort(arr, maxDigits);
 
-	cin >> n;
-
-	int num;
-
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-
-		arr.push_back(num);
-	}
-	radix_sort(6);
-
-	for (int e : arr)
-		cout << e << ' ';
-
-
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
 }
